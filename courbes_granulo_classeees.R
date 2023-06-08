6fe78978ea9f5e6f38ab42bd9c1e972fc2e782b2
VigneronT <thibault.vigneron@ofb.gouv.fr>
2023-06-08 17:51:52 +0200

    mise à jour du script avec D16-D50 et D84

--- courbes_granulo_classeees.R
+++ courbes_granulo_classeees.R
@@ -17,20 +17,28 @@ effectifs <- table(wolman_2015_aval)
 View(effectifs)
 class(effectifs)
 #transformer la table en data frame
+
 dataframe_frequence <- as.data.frame(effectifs)
 print(dataframe_frequence)
 
-#########cumul des freq#########
+### Cumul des freq ######
 
-wolman_cumul <- data.frame(X = dataframe_frequence$Diam, Y=cumsum(dataframe_frequence$Freq))
+wolman_cumul <- data.frame(Diam = dataframe_frequence$Diam, Freq=cumsum(dataframe_frequence$Freq))
 print(wolman_cumul)
 
 print(wolman_cumul)
+
+write.csv(wolman_cumul,file = "raw_data/wolman_cumul.csv", row.names = FALSE)
 # Tracer la courbe de granulométrie classée
 
-ggplot(data=wolman_cumul, aes(x = factors(Diam), y = Freq)) +
+ggplot(data=wolman_cumul, aes(x = Diam, y = Freq,group=1)) +
   geom_line() +
   labs(x ="Diametre", y = "Pourcentage cumulatif", title = "Courbe de granulométrie classée")
 
-usethis::use_github()
+########   médiane 
+
+median(wolman_cumul$Y)
+
+quantile(x = wolman_2015_aval$Diam, 
+         probs = c(0.16, 0.5, 0.86))
 
